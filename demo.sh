49911479cb4f64f770a93b9b27b1a634fc7e5d2d
Carter Morgan <askcarter@google.com>
2016-05-20 11:01:41 -0700

    Updated demo script to automatically grab the frontend's External IP

--- demo.sh
+++ demo.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+# Grab the frontends's External IP address using a template so I can curl it later.
+IP=$(kubectl get svc frontend -o template --template "{{with index .status.loadBalancer.ingress 0}}{{.ip}}{{end}}")
+
 while true
 do
-  curl -k https://$1
+  curl -k https://$IP
   sleep .3
 done
 
